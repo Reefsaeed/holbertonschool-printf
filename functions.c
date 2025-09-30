434b3c7d94a35957b46f8fdb6349b3375e7c9164
Nawaf <Alzhraniii999@gmail.com>
2025-09-30 10:30:17 +0000

    Add new function named print_string that prints strings from the user

--- functions.c
+++ functions.c
@@ -8,8 +8,19 @@ return (0);
 /* Placeholder for %s */
 int print_string(va_list args)
 {
-/* TODO: implement %s */
-return (0);
+
+char *str = va_arg(args, char*);
+int i = 0;
+if (str == NULL)
+    str = "(null)";
+
+while (str[i])
+{
+    write(1, &str[i], 1);
+    i++;
+}
+
+return (i);
 }
 /* Placeholder for %% */
 int print_percent(va_list args)
