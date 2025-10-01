1dfc019f342449b9bcdfc8fac4f8a4e5a3f0fecf
Nawaf <Alzhraniii999@gmail.com>
2025-10-01 08:11:37 +0000

    I fixed betty style issues

--- _printf.c
+++ _printf.c
@@ -1,44 +1,50 @@
 #include "main.h"
+
 /**
-* _printf - Custom printf function (skeleton)
-* @format: Format string
-*
-* Return: Number of characters printed
-*/
+ * _printf - Custom printf function (skeleton)
+ * @format: Format string
+ *
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-int count = 0;
-va_list args;
-if (format == NULL)
-return (-1);
-va_start(args, format);
-while (*format)
-{
-    if (*format == '%')
-    {
-        format++;
+	int count = 0;
+	va_list args;
 
-        if (*format == '\0')
-            return (-1);
+	if (format == NULL)
+		return (-1);
 
-        if (*format == 'c')
-            count += _putchar(va_arg(args, int));
-        else if (*format == 's')
-            count += print_string(args);
-        else if (*format == '%')
-            count += _putchar('%');
-        else
-        {
-            count += _putchar('%');
-            count += _putchar(*format);
-        }
-    }
-    else
-    {
-        count += _putchar(*format);
-    }
-    format++;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == '\0')
+				return (-1);
+
+			if (*format == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += print_string(args);
+			else if (*format == '%')
+				count += _putchar('%');
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(*format);
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
-va_end(args);
-return (count);
-}
+
